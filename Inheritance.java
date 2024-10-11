a904b15edd22762bc797ad1e1a215a7828733ff1
paellpunya <152680210+paellpunya@users.noreply.github.com>
2024-10-11 13:46:19 +0700

    Add files via upload

---
+++ Inheritance.java
@@ -0,0 +1,11 @@
+class Mobil extends Kendaraan {
+    // Kode kelas Mobil
+}
+
+class Motor extends Kendaraan {
+    // Kode kelas Motor
+}
+
+class Truk extends Kendaraan {
+    // Kode kelas Truk
+}
