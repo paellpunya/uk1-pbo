9800121af2a74e014520b0d9844542b2efb77e4a
paellpunya <152680210+paellpunya@users.noreply.github.com>
2024-10-11 13:45:34 +0700

    Add files via upload

---
+++ contruksi.java
@@ -0,0 +1,9 @@
+public Kendaraan() {
+    this.merk = "Tidak diketahui";
+    this.tahunProduksi = 0;
+}
+
+public Kendaraan(String merk, int tahunProduksi) {
+    this.merk = merk;
+    this.tahunProduksi = tahunProduksi;
+}
