92376ad27dfcab180253f6c7133d20a437db56e9
paellpunya <152680210+paellpunya@users.noreply.github.com>
2024-10-11 13:47:19 +0700

    Add files via upload

---
+++ Setter dan Getter.java
@@ -0,0 +1,15 @@
+public String getMerk() {
+    return merk;
+}
+
+public void setMerk(String merk) {
+    this.merk = merk;
+}
+
+public int getTahunProduksi() {
+    return tahunProduksi;
+}
+
+public void setTahunProduksi(int tahunProduksi) {
+    this.tahunProduksi = tahunProduksi;
+}
